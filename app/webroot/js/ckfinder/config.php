507db06695ab9c6c789cc96f6bf4e256e235f489
jung@adspace.jp <j@jslc.jp>
2011-07-23 05:42:59 +0900

    config chagee

--- app/webroot/js/ckfinder/config.php
+++ app/webroot/js/ckfinder/config.php
@@ -61,7 +61,7 @@ Examples:
 
 ATTENTION: The trailing slash is required.
 */
-$baseUrl = '/img/';
+$baseUrl = '/app/webroot/';
 
 /*
 $baseDir : the path to the local directory (in the server) which points to the
